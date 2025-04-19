faf319a884e186d0b219c9e6f2d90f964b57f9f5
Daniel Soares <danielsoares300907@gmail.com>
2025-04-19 15:49:23 -0300

    Página 40

---
+++ Pág 40 (a)/script.js
@@ -0,0 +1,8 @@
+let valor1 = parseInt(prompt("Digite o primeiro valor"))
+let valor2 = parseInt(prompt("Digite o segundo valor"))
+
+if (valor1 > valor2) {
+    alert("A diferença do maior valor pro menor valor é: " + (valor1 - valor2) )
+} else {
+    alert("A diferença do maior valor pro menor valor é: " + (valor2 - valor1) ) 
+}
